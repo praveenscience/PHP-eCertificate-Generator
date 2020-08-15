30aa04be75ea3c50545d516509451cfb52598cc6
Praveen Kumar <praveenscience@gmail.com>
2020-08-15 12:42:31 +0100

    Used a custom font to render text.

--- index.php
+++ index.php
@@ -5,8 +5,13 @@
 	$image = imagecreatefrompng("empty.png");
 	// Create a text colour.
 	$textColour = imagecolorallocate($image, 0, 0, 0);
+	// Get the font path.
+	$fontPath = __DIR__ . "/font.otf";
+	// Import the custom font from path.
 	// Write text inside image.
-	imagestring($image, 5, 1000, 700, $name, $textColour);
+	// Left margin should be negated with half width of the text.
+	// Current text width is 575px, so divided by 2 comes around 285.
+	imagettftext($image, 60, 0, 1000 - (575 / 2), 700, $textColour, $fontPath, $name);
 	// Instruct the browser to read this page as image.
 	header("Content-type: image/png");
 	// Show the image to the browser or output.
